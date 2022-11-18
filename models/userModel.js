9dffce35bf37dcf9b9b987db2aab946a80a20b5c
readul-islam <developer.readul@gmail.com>
2022-11-18 22:38:56 +0600

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,23 +1,16 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        maxLength:[20,'Maximum length 20']
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:[true,'Email already exist' ]
-    },
-    password:{
-        type:String,
-        required:true,
-        minLength:[5,'Minimum length 5']
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email already exist"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: [5, "Minimum length 5"],
+  },
+});
 
-
-module.exports= mongoose.model('user', userSchema)
+module.exports = mongoose.model("user", userSchema);
